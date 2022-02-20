5a6d72c5412980aa8efe5e3a43e400d36db69a41
DarthVauban <Dante2770@gmail.com>
2022-02-20 22:57:24 +0200

    Добавил сокрытие формы

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const loginForm = document.querySelector('.form-content')
+const regLink = document.querySelector('.sign-up')
+
+regLink.addEventListener('click', () => {
+ loginForm.style.display = 'none'
+})
