613206d0ead81c8737b396872db7c045540c0682
DarthVauban <Dante2770@gmail.com>
2022-03-08 23:28:51 +0200

    Готовая форма логина и регистрации

--- js/main.js
+++ js/main.js
@@ -1,6 +1,65 @@
-const loginForm = document.querySelector('.form-content')
+const loginForm = document.querySelector('.form-block')
 const regLink = document.querySelector('.sign-up')
+const regForm = document.querySelector('.reg-form')
+const regButton = document.querySelector('.reg-button')
+const formSteps = document.querySelectorAll('.form-step')
+const stepItems = document.querySelectorAll('.steps .step-item')
+const formTitle = document.querySelector('.reg-title')
+const logLink = document.querySelector('.sign-in')
+const stepForm = document.querySelector('.step-form')
+
+
+let formStepCounter = 0
 
 regLink.addEventListener('click', () => {
- loginForm.style.display = 'none'
+    loginForm.style.display = 'none'
+    stepForm.style.display = 'block'
 })
+
+logLink.addEventListener('click', () => {
+    loginForm.style.display = 'flex'
+    stepForm.style.display = 'none'
+})
+
+regForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
+
+regButton.addEventListener('click', () => {
+    formStepCounter++
+    updateFormSteps()
+})
+
+function updateFormSteps() {
+    formSteps.forEach((step) => {
+        step.classList.contains('form-step-active') && step.classList.remove('form-step-active')
+    })
+
+    if (formStepCounter === formSteps.length) {
+        alert('Registration success')
+        loginForm.style.display = 'flex'
+        stepForm.style.display = 'none'
+    }
+
+    formSteps[formStepCounter].classList.add('form-step-active')
+
+    stepItems[formStepCounter].classList.add('step-active')
+
+    switch (formStepCounter) {
+        case 0:
+            formTitle.innerHTML = 'Basic information'
+            break;
+        case 1:
+            formTitle.innerHTML = 'Additional information'
+            break;
+
+        case 2:
+            formTitle.innerHTML = 'Interests'
+            regButton.innerHTML = 'SIGN UP!'
+            break;
+
+        default:
+            formTitle.innerHTML = 'Basic information'
+    }
+
+}
